44866fa97ea7939fca29a96c9afe302416e08018
Bertalan Körmendy <korber314@gmail.com>
2020-12-06 12:51:50 +0100

    add nyereseg function

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -1,9 +1,26 @@
 import math
+from collections import namedtuple
+
+from typing import List
+
+NodeData = namedtuple("NodeData", "p, n")
 
 
-def I(p: float, n: float) -> float:
+def I(data: NodeData) -> float:
     """Információ-igény"""
-    p_over = (p / (p + n))
-    n_over = (n / (p + n))
+    if data.n == 0 or data.p == 0:
+        return 0
+    p_over = (data.p / (data.p + data.n))
+    n_over = (data.n / (data.p + data.n))
     return - p_over * math.log(p_over, 2) - n_over * math.log(n_over, 2)
 
+
+def Nyereseg(data: List[NodeData]) -> float:
+    p_total = sum(map(lambda d: d.p, data))
+    n_total = sum(map(lambda d: d.n, data))
+
+    result = 0
+    for subtree in data:
+        result += (subtree.p + subtree.n) / (p_total + n_total) * I(subtree)
+
+    return result
--- src/decision_tree_test.py
+++ src/decision_tree_test.py
@@ -1,7 +1,15 @@
 import unittest
-from .decision_tree import *
+from .decision_tree import I, NodeData, Nyereseg
 
 
 class MyTest(unittest.TestCase):
-    def test(self):
-        self.assertAlmostEqual(I(1070, 990), 0.9989, 4)
+    def test_I(self):
+        """Tesztesetek a 2. zh-ból"""
+        self.assertAlmostEqual(I(NodeData(1070, 990)), 0.9989, 4)
+        self.assertAlmostEqual(I(NodeData(490, 174)), 0.8298, 4)
+        self.assertAlmostEqual(I(NodeData(230, 0)), 0, 4)
+        self.assertAlmostEqual(I(NodeData(350, 816)), 0.8815, 4)
+
+    def test_Nyereseg(self):
+        """Tesztesetek a 2. zh-ból"""
+        self.assertAlmostEqual(Nyereseg([NodeData(230, 0), NodeData(490, 174), NodeData(350, 816)]), 0.7664, 4)
