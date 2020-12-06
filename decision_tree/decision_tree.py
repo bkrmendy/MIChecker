da19cd8d81c45399197ae32b967e8723ac7faa25
Bertalan Körmendy <korber314@gmail.com>
2020-12-06 12:21:34 +0100

    project start

---
+++ decision_tree/decision_tree.py
@@ -0,0 +1,9 @@
+import math
+
+
+def I(p: float, n: float):
+    """Információ-igény"""
+    p_over = (p / (p + n))
+    n_over = (n / (p + n))
+    return - p_over * math.log(p_over, 2) - n_over * math.log(n_over, 2)
+
