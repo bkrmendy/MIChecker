0f291e160c5ab9755910bf8d3fbb2f0ae9fd3463
Bertalan Körmendy <korber314@gmail.com>
2020-12-16 22:51:55 +0100

    add comments

--- src/active_reinforcement.py
+++ src/active_reinforcement.py
@@ -5,9 +5,11 @@ Edge = namedtuple("Edge", "weight, value")
 
 
 def utility(edges: List[Edge]):
+    """Calculates utility for action, based on edges pointing out from a starting position"""
     return sum(map(lambda e: e.weight * e.value, edges))
 
 
-def next_utility(reward: float, leszamitolas: float, expected_utility: float):
-    return  reward + leszamitolas * expected_utility
+def next_utility(reward: float, leszamitolas: float, max_expected_utility: float):
+    """Calculates next utility value based on first two parameters and max expected utility"""
+    return reward + leszamitolas * max_expected_utility
 
--- src/active_reinforcement_tests.py
+++ src/active_reinforcement_tests.py
@@ -15,7 +15,7 @@ class PerceptronTestCases(unittest.TestCase):
         self.assertAlmostEqual(expected_utility, 1.11, 2)
 
     def test_utility_a2(self):
-        """2. ZH 20.feladat"""
+        """2. ZH 21.feladat"""
         edges = [
             Edge(0.3, -5),  # nyíl s1-ből s1-be
             Edge(0.7, -1)   # nyíl s1-ből s2-be
@@ -24,6 +24,7 @@ class PerceptronTestCases(unittest.TestCase):
         self.assertAlmostEqual(expected_utility, -2.2, 1)
 
     def test_next_utility(self):
+        """2. ZH 23.feladat"""
         edges_a1 = [
             Edge(0.5, -1),      # nyíl s1-ből s1-be
             Edge(0.1, -5),      # nyíl s1-ből s2-be
